52ed34d52e795392efa42925ab89d886e982212c
iansincolorado <iansincolorado@gmail.com>
2024-01-19 16:27:19 -0700

    watched Neetcode solution

--- NeetCode150/DynamyicProgramming/0070ClimbingStairs.py
+++ NeetCode150/DynamyicProgramming/0070ClimbingStairs.py
@@ -21,3 +21,15 @@ class Solution(object):
             dp[i] = dp[i+1] + dp[i+2]
 
         return dp[0]
+
+        '''
+        # NeetCode Solution without using an array
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        return one
+        '''
