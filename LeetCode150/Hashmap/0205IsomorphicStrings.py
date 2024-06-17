3ce95ff2b1582af9abf61438dbb9af6776712eee
IanSinColorado <iansincolorado@gmail.com>
2024-06-17 15:06:03 -0600

    Doing some extra LeetCode

--- LeetCode150/Hashmap/0205IsomorphicStrings.py
+++ LeetCode150/Hashmap/0205IsomorphicStrings.py
@@ -9,11 +9,15 @@ class Solution(object):
             return False
 
         sHash = {}
+        used = []
         for i in range(len(s)):
             if s[i] in sHash:
                 if sHash[s[i]] != t[i]:
                     return False
             else:
+                if t[i] in used:
+                    return False
                 sHash[s[i]] = t[i]
+                used.append(t[i])
 
         return True
