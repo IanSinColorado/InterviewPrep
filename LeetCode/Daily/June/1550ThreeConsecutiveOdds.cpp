b86b96146fbcf1e8efbaa155b458be680719e7e4
IanSinColorado <iansincolorado@gmail.com>
2024-07-01 14:00:05 -0600

    Daily: 1550

---
+++ LeetCode/Daily/June/1550ThreeConsecutiveOdds.cpp
@@ -0,0 +1,19 @@
+# include <vector>
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int counter = 0;
+        for(int i = 0; i < arr.size(); ++i) {
+            if (arr[i] % 2 == 0) {
+                counter = 0;
+            } else {
+                if (counter == 2){
+                    return true;
+                }
+                counter += 1;
+            }
+        }
+        return false;
+    }
+};
